a864e46fbd260ac0c145da777b3449bccf7b03f0
Haritha <Harimigara@gmail.com>
2024-01-29 11:33:01 +0530

    done

--- src/Pages/Checkout.js
+++ src/Pages/Checkout.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
 import { BiArrowBack } from "react-icons/bi";
+import watch from "../images/watch.jpg";
 
 const Checkout = () => {
   return (
@@ -33,7 +34,7 @@ const Checkout = () => {
                     </li>
                   </ol>
                 </nav>
-                <h4 className="title">Contaact Information</h4>
+                <h4 className="title">Contact Information</h4>
                 <p className="user-details">eHelp customer (eHelp@gmail.com)</p>
                 <form
                   action=""
@@ -116,23 +117,44 @@ const Checkout = () => {
             </div>
 
             <div className="col-5">
-              <div className="border-bottom py-4"></div>
+              <div className="border-bottom py-4">
+                <div className="d-flex gap-10 align-items-center">
+                  <div className="w-75 d-flex gap-10">
+                    <div className="w-25 position-relative">
+                      <span
+                        style={{ top: "-10px", right: "2px" }}
+                        className="badge bg-secondary text-white rounded-circle p-2 position-absolute"
+                      >
+                        1
+                      </span>
+                      <img src={watch} className="img-fluid" alt="product" />
+                    </div>
+                    <div>
+                      <h5 className="title">smart watch 2023 barnd new</h5>
+                      <p>S / #6767r</p>
+                    </div>
+                  </div>
+                  <div className="flex-grow-1">
+                    <h5>$ 100</h5>
+                  </div>
+                </div>
+              </div>
 
               <div className="border-bottom py-4">
                 <div className="d-flex justify-content-between align-items-center">
-                  <p>subtotal</p>
-                  <p>$ 250</p>
+                  <p className="total">subtotal</p>
+                  <p className="total-price">$ 250</p>
                 </div>
 
                 <div className="d-flex justify-content-between align-items-center">
-                  <p>Shipping</p>
-                  <p>$ 25</p>
+                  <p className="mb-0 total">Shipping</p>
+                  <p className="mb-0 total-price">$ 25</p>
                 </div>
               </div>
 
               <div className="d-flex justify-content-between align-items-center border-bottom py-4">
-                <h4>total</h4>
-                <h5>$ 300</h5>
+                <h4 className="total">total</h4>
+                <h5 className="total-price">$ 300</h5>
               </div>
             </div>
           </div>
